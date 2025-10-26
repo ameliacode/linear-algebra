7b0aa88f58567457d914858d92510cc9d86a4a52
ameliacode <ameliacode@hanyang.ac.kr>
2025-10-27 00:55:45 +0900

    [Task] Update task 1.6

--- 01-function/1-6.py
+++ 01-function/1-6.py
@@ -1,5 +1,25 @@
+import cmath
 import math
-from random import randint
 
 # Task 1.6.1
-print(math.sqrt(-1), math.cos(math.pi), math.log(math.e))
+print(cmath.sqrt(-1), math.cos(math.pi), math.log(math.e))
+
+
+# Task 1.6.2
+from random import randint
+
+
+def movie_review(name: str) -> str:
+    movie_reviews = ["See it!", "A gem!", "Ideological claptrapl!"]
+    return f"{movie_reviews[randint(0, len(movie_reviews) - 1)]} {name} {movie_reviews[randint(0, len(movie_reviews) - 1)]}"
+
+
+print(movie_review("Interstellar"))
+
+# Task 1.6.3
+# from imp import reload -> DeprecationWarning: removal in Python 3.12
+
+# Task 1.6.4
+from dictutil import listrange2dict
+
+print(listrange2dict(["A", "B", "C"]))
---
+++ 01-function/dictutil.py
@@ -0,0 +1,11 @@
+# Copyright 2013 Philip N. Klein
+def dict2list(dct, keylist):
+    pass
+
+
+def list2dict(L, keylist):
+    pass
+
+
+def listrange2dict(L: list) -> dict:
+    return {i: L[i] for i in range(len(L))}

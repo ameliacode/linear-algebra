b2ad9028bf456c07ac95a48955aebbdede44edfc
ameliacode <ameliacode@hanyang.ac.kr>
2025-11-02 22:52:39 +0900

    [Task] Update problem 2.5

--- 02-fields/2-5.py
+++ 02-fields/2-5.py
@@ -1,16 +1,20 @@
-# Problem 2.5.1
-from GF2 import one
+# Problem 2.5.1 eve is evil 10001
 
 
-def binary2digit(seq: str, key: str) -> int:
-    
+def binary2digit(seq: str) -> int:
+    return int(seq, 2)
 
 
-def decode(code: str) -> str:
+def cyper2plain(seq: str, key: str) -> str:
+    return "".join("1" if s != k else "0" for s, k in zip(seq, key))
+
+
+def decrypt(cypertext: str, key: str) -> str:
     answer = []
-    symbols = code.split()
+    symbols = cypertext.split()
     for symbol in symbols:
-        digit = binary2digit(symbol)
+        plain = cyper2plain(symbol, key)
+        digit = binary2digit(plain)
         if digit == 26:
             answer.append(" ")
         else:
@@ -19,5 +23,8 @@ def decode(code: str) -> str:
     return "".join(answer)
 
 
-code = "10101 00100 10101 01011 11001 00011 01011 10101 00100 11001 11010"
-print(decode(code))
+cypertext = "10101 00100 10101 01011 11001 00011 01011 10101 00100 11001 11010"
+keys = [f"{i:05b}" for i in range(32)]
+
+for key in keys:
+    print(decrypt(cypertext, key), key)
